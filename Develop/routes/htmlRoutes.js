9b509006ac2416c102251601c85f039de696c68b
Karen Astell <KarenAstell@gmail.com>
2020-06-09 17:48:51 -0400

    htmlRoutes.js has routes that work to each of the html pages

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,13 +1,22 @@
 // install your packages which may include: path, express (router from express)
 const router = require("express").Router();
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 // get /notes route 
 // sendFile() - directory, filename you want to display
+router.get("/notes", (req, res)=>{
+   const filePath = path.join(__dirname, "..", "public", "notes.html")
+    res.sendfile(filePath)
+})
 
 // get "*" route
+router.get("/", (req, res)=>{
+   const filePath = path.join(__dirname, "..", "public", "index.html")
+    res.sendfile(filePath)
+})
 // sendFile() - directory, filename you want to display
 
 // export the Router
