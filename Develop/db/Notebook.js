8cba421e9509bbe01d3d4cda6ec6a61a2b76a729
Karen Astell <KarenAstell@gmail.com>
2020-06-10 09:47:31 -0400

    started on the functions in Notebook class

--- Develop/db/Notebook.js
+++ Develop/db/Notebook.js
@@ -1,15 +1,28 @@
 // require uuidv4 package to generate an {id : uuidv4()}  in the object with title and text info
-// const uuidv4 = require("uuidv4");
+const { uuid } = require("uuidv4");
+const fs = require("fs");
+
 
 class Notebook {
   getNotes() {
     // want to READ from the db.json file
+    fs.readFile("./db.json", (err, data)=>{
+      if(err) throw err;
+      console.log(data);
+      
+      
+    })
     // the contents of the json file will be displayed on the page
     // to read the file - readFile() which is a parat of the 'fs' package
   }
 
   addNotes() {
-     // want to write to the db.json file using writeFile() from the 'fs' package 
+     // want to write to the db.json file using writeFile() from the 'fs' package
+     fs.writeFile("./db.json", (err, data)=>{
+       if (err) throw err;
+       console.log(data);
+       
+     })
   }
 
 
